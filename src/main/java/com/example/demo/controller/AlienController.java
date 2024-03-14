9ffe21a4d8ed77ca882196ce7297a4ed4fb9f771
pranjalad12 <lci2021021@iiitl.ac.in>
2024-03-14 23:37:04 +0530

    added tomcat dependency

--- src/main/java/com/example/demo/controller/AlienController.java
+++ src/main/java/com/example/demo/controller/AlienController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AlienController {
 
-	@RequestMapping
+	@RequestMapping("/")
 	public String home() {
 		
 		return "home.jsp";
